38fedd042ecceeed7a113fbafe8f981fba6169e4
GreyStinger <jaydenandrews010@gmail.com>
2021-12-13 00:36:27 +0200

    And just like that I have acomplished nothing

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,15 +1,7 @@
 import pytest
-
-from src import silicium
-from src import silicium_web
-
-class TestSiliciumMain:
-    def test_builder(x):
-        assert True
-
 class TestSiliciumWeb:
-    def test_html(x):
+    def test_html(e):
         assert True
 
-    def test_css(x):
+    def test_css(e):
         assert True
