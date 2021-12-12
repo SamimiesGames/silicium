3402e4d93b30b915472b421b0bbf3e58ffb15f0f
SamimiesGames <sami.laatikainen@saunalahti.fi>
2021-12-12 14:22:31 +0200

    `feat(demo page)`

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from silicium_web import Text, HTML, DefaultTheme
 html = HTML()
 
 DefaultTheme(html)
-Text(html, text="Hello, Silicium!")
-Text(html, text="Silicium is a progressive, infinitely scalable Python web-framework.")
+Text(html, text="Hello, Silicium-web!")
+Text(html, text="Silicium-web is a progressive, infinitely scalable Python web-framework.")
 
 html.run()
-
