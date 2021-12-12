aef6611a4b57081700e64f242674ccdbc1310927
GreyStinger <jaydenandrews010@gmail.com>
2021-12-12 22:18:57 +0200

    Start with tests

---
+++ tests/test_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from ..src import silicium
+from ..src import silicium_web
+
+class TestSiliciumMain:
+    def test_builder():
+        pass
+
+class TestSiliciumWeb:
+    def test_html():
+        pass
+
+    def test_css():
+        pass
