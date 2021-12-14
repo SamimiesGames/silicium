295510e4a463211f59060700149d7535158d358e
GreyStinger <jaydenandrews010@gmail.com>
2021-12-14 15:40:40 +0200

    made explicit variable, removed redundant code

--- src/silicium_web/lang/css/default.py
+++ src/silicium_web/lang/css/default.py
@@ -6,10 +6,8 @@ from . import optimizer
 class DefaultTheme(Component):
     build_target = "css"
 
-    def __init__(self, parent, add: bool = True, **kwargs):
-        super().__init__(parent, add=add, **kwargs)
-
     @property
     def code(self) -> str:
-        css_optimized = optimizer.min_css(require(__file__, "themes.css"))
+        css = require(__file__, "themes.css")
+        css_optimized = optimizer.min_css(css)
         return f"<style>{css_optimized}</style>"
