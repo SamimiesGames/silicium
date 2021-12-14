df0fb8409803d198fecec03ab6b90c0e8bba0b11
GreyStinger <jaydenandrews010@gmail.com>
2021-12-14 12:59:09 +0200

    Added comments on update of list to typing List

--- src/silicium/builder.py
+++ src/silicium/builder.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from dataclasses import dataclass, field
+# Added list for typing for back compatibility to 3.8 and 3.7
 from typing import Union, List
 
 from .component import AbstractComponent
@@ -9,6 +10,7 @@ from .component import AbstractComponent
 @dataclass
 class AbstractBuilder(ABC):
     builder_component: Union[None, AbstractComponent] = None
+    # use List instead of list for typehint for py37 and py38
     components: List[AbstractComponent] = field(default_factory=list)
 
     @abstractmethod
